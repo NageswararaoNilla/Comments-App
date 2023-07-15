33bed789a9f53c3f893f9e26c405032b83e77658
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-07-15 11:13:48 +0000

    wrote code for dynamically add the comments and comments count

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,54 @@
-// Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {commentDetails, bgColors} = props
+  const {name, comment, timeUpdate, isLiked} = commentDetails
+  //   console.log(timeUpdate)
+  const initial = name[0]
+
+  const getBgColor = () => {
+    const index = Math.floor(Math.random() * bgColors.length)
+    const bgColor = bgColors[index]
+    return bgColor
+  }
+
+  const likeImg = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+
+  const likeColor = isLiked ? 'blue-text' : ''
+
+  return (
+    <li className="comment-container">
+      <div className="initial-comment-container">
+        <div className="initial-container">
+          <p className={`initial ${getBgColor()}`}>{initial}</p>
+        </div>
+        <div>
+          <div className="name-time">
+            <h1 className="name">{name}</h1>
+            <p className="time">{timeUpdate}</p>
+          </div>
+          <p className="comment-text">{comment}</p>
+        </div>
+      </div>
+      <div className="like-btn-container">
+        <div className="like-img-container">
+          <img src={likeImg} alt="like" className="like-img" />
+          <button type="button" className={`like-btn ${likeColor}`}>
+            Like
+          </button>
+        </div>
+        <button type="button" className="delete-btn" data-testid="delete">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png "
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,5 +1,11 @@
 import {Component} from 'react'
 
+import {v4 as uuidv4} from 'uuid'
+
+import {formatDistanceToNow} from 'date-fns'
+
+import CommentItem from '../CommentItem'
+
 import './index.css'
 
 const initialContainerBackgroundClassNames = [
@@ -12,29 +18,59 @@ const initialContainerBackgroundClassNames = [
   'light-blue',
 ]
 
+const initialComments = [
+  {
+    id: uuidv4(),
+    name: 'Richard Branson',
+    comment:
+      'Thanks for being so typically supportive and such a good friend,Elon.Great to be opening up space for all.',
+    timeUpdate: formatDistanceToNow(new Date()),
+    isLiked: false,
+  },
+]
+
 // Write your code here
 const commentImg =
   'https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png'
 
 class Comments extends Component {
-  state = {name: '', comment: ''}
+  state = {commentsList: initialComments, name: '', comment: ''}
 
-  getBgColor = () => {
-    const index = Math.floor(
-      Math.random() * initialContainerBackgroundClassNames.length,
-    )
-    const bgColor = initialContainerBackgroundClassNames[index]
-    return bgColor
+  onSubmitComment = event => {
+    const {name, comment} = this.state
+    event.preventDefault()
+    const timeUpdate = formatDistanceToNow(new Date())
+    const newComment = {
+      id: uuidv4(),
+      name,
+      comment,
+      timeUpdate,
+      isLiked: false,
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      name: '',
+      comment: '',
+    }))
   }
 
-  onSubmit = event => {
-    event.preventDefault()
+  onName = event => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
+  onComment = event => {
+    this.setState({
+      comment: event.target.value,
+    })
   }
 
   render() {
-    const {name, comment} = this.state
-    const bgColor = this.getBgColor()
-    console.log(name, comment, bgColor)
+    const {commentsList} = this.state
+    const len = commentsList.length
+    // const bgColor = this.getBgColor()
+    // console.log(commentsList, bgColor)
     return (
       <div className="app-container">
         <div className="comments-banner-container">
@@ -47,19 +83,21 @@ class Comments extends Component {
               <p className="description">
                 Say something about 4.0 Technologies
               </p>
-              <form className="form-container" onClick={this.onSubmit}>
+              <form className="form-container" onSubmit={this.onSubmitComment}>
                 <input
                   type="text"
                   placeholder="Your Name"
                   className="user-name"
+                  onChange={this.onName}
                 />
                 <textarea
                   rows="5"
                   cols="50"
                   placeholder="Your Comment"
                   className="input-comment"
+                  onChange={this.onComment}
                 />
-                <button type="button" className="btn">
+                <button type="submit" className="btn">
                   Add Comment
                 </button>
               </form>
@@ -69,10 +107,16 @@ class Comments extends Component {
         <hr className="hr-line" />
         <div className="comments-list-container">
           <p className="count-comments">
-            <span className="count">0</span>Comments
+            <span className="count">{len}</span>Comments
           </p>
           <ul className="comments-list">
-            <li>Comment</li>
+            {commentsList.map(eachComment => (
+              <CommentItem
+                commentDetails={eachComment}
+                key={eachComment.id}
+                bgColors={initialContainerBackgroundClassNames}
+              />
+            ))}
           </ul>
         </div>
       </div>
